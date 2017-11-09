9612519529c0e49b4fe29f75251e564991c5bd1c
Tural Gurbanov <tural.gurbanov@inf.unibz.it>
2017-11-09 15:25:53 +0100

    Right file paths for production

--- hh/settings/production.py
+++ hh/settings/production.py
@@ -2,6 +2,6 @@ from hh.settings.base import *
 
 # file paths
 API_DATA_DIR = "/www/clustered_cars/data/"
-USER_CLUSTERS_JSON_PATH = os.path.join(API_DATA_DIR, "clustered/user.txt")
-BOOKING_CSV_PATH = os.path.join(API_DATA_DIR, "cleaned/booking.csv")
-ITEM_CSV_PATH = os.path.join(API_DATA_DIR, "cleaned/property.csv")
+USER_CLUSTERS_JSON_PATH = os.path.join(API_DATA_DIR, "user.txt")
+BOOKING_CSV_PATH = os.path.join(API_DATA_DIR, "booking.csv")
+ITEM_CSV_PATH = os.path.join(API_DATA_DIR, "property.csv")
