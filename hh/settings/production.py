5716caf9f7b07091c299fa2845f86452b2dfae6c
Tural Gurbanov <tural.gurbanov@inf.unibz.it>
2017-11-09 17:23:13 +0100

    config and requirements

--- hh/settings/production.py
+++ hh/settings/production.py
@@ -5,3 +5,6 @@ API_DATA_DIR = "/www/clustered_cars/data/"
 USER_CLUSTERS_JSON_PATH = os.path.join(API_DATA_DIR, "user.txt")
 BOOKING_CSV_PATH = os.path.join(API_DATA_DIR, "booking.csv")
 ITEM_CSV_PATH = os.path.join(API_DATA_DIR, "property.csv")
+
+ALLOWED_HOSTS = ['46.18.25.101']
+DEBUG = True
