b8dce93b2915c58977b9b929fcf16400220ae27d
Tural Gurbanov <tural.gurbanov@inf.unibz.it>
2017-11-18 20:29:57 +0100

    Updates to the production settings

--- hh/settings/production.py
+++ hh/settings/production.py
@@ -1,3 +1,5 @@
+import json
+
 from hh.settings.base import *
 
 # file paths
@@ -5,6 +7,10 @@ API_DATA_DIR = "/www/clustered_cars/data/"
 USER_CLUSTERS_JSON_PATH = os.path.join(API_DATA_DIR, "user.txt")
 BOOKING_CSV_PATH = os.path.join(API_DATA_DIR, "booking.csv")
 ITEM_CSV_PATH = os.path.join(API_DATA_DIR, "property.csv")
+
+PROPERTY_DESCR_PATH = os.path.join(API_DATA_DIR, "property_descrs.json")
+with open(PROPERTY_DESCR_PATH) as f:
+    PROPERTY_DESCR = json.load(f)
 
 ALLOWED_HOSTS = ['46.18.25.101']
 DEBUG = False
